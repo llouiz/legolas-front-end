89b40144e8478e03ec1ff8b1725069471c6c1c03
Luiz Carlos <luizcarlos--1@hotmail.com>
2020-08-11 11:27:57 -0300

    faq

--- src/components/faq.js
+++ src/components/faq.js
@@ -5,56 +5,56 @@ class FAQ extends Component {
         return(
             <div>
                 <section id="container">
-                <header class="header black-bg">
-                    <div class="sidebar-toggle-box">
-                        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+                <header className="header black-bg">
+                    <div className="sidebar-toggle-box">
+                        <div className="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
                     </div>
-                    <a href="/" class="logo"><b>Lego<span>las</span></b></a>
+                    <a href="/" className="logo"><b>Lego<span>las</span></b></a>
 
-                    <div class="top-menu">
-                        <ul class="nav pull-right top-menu">
-                        <li><a class="logout" href="/">Logout</a></li>
+                    <div className="top-menu">
+                        <ul className="nav pull-right top-menu">
+                        <li><a className="logout" href="/">Logout</a></li>
                         </ul>
                     </div>
                     </header>
 
                     <aside>
-                    <div id="sidebar" class="nav-collapse ">
-                        <ul class="sidebar-menu" id="nav-accordion">
+                    <div id="sidebar" className="nav-collapse ">
+                        <ul className="sidebar-menu" id="nav-accordion">
 
-                        <li class="mt">
+                        <li className="mt">
                             <a href="/">
-                            <i class="fa fa-dashboard"></i>
+                            <i className="fa fa-dashboard"></i>
                             <span>Dashboard</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="javascript:;">
-                            <i class="fa fa-calendar"></i>
+                            <i className="fa fa-calendar"></i>
                             <span>Calendar</span>
                             </a>
-                            <ul class="sub">
+                            <ul className="sub">
                             <li><a href="/calendar">Events</a></li>
                             </ul>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="/gallery">
-                            <i class="fa fa-picture-o"></i>
+                            <i className="fa fa-picture-o"></i>
                             <span>Gallery</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="/contact">
-                            <i class="fa fa-tasks"></i>
+                            <i className="fa fa-tasks"></i>
                             <span>Contact</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="javascript:;">
-                            <i class="fa fa-th"></i>
+                            <i className="fa fa-th"></i>
                             <span>Tables</span>
                             </a>
-                            <ul class="sub">
+                            <ul className="sub">
                             <li>
                                 <a href="/">Players</a>
                             </li>
@@ -63,9 +63,9 @@ class FAQ extends Component {
                             </li>
                             </ul>
                         </li>
-                        <li class="sub-menu">
-                            <a class="active" href="/faq">
-                            <i class="fa fa-book"></i>
+                        <li className="sub-menu">
+                            <a className="active" href="/faq">
+                            <i className="fa fa-book"></i>
                             <span>FAQ</span>
                             </a>
                         </li>
@@ -74,35 +74,35 @@ class FAQ extends Component {
                     </aside>
 
                     <section id="main-content">
-                    <section class="wrapper site-min-height">
-                        <div class="row mt mb">
-                        <div class="col-lg-12">
-                            <h3><i class="fa fa-angle-right"></i> F.A.Q</h3>
+                    <section className="wrapper site-min-height">
+                        <div className="row mt mb">
+                        <div className="col-lg-12">
+                            <h3><i className="fa fa-angle-right"></i> F.A.Q</h3>
                             <br/>
-                            <div class="col-lg-4 col-md-4 col-sm-12">
-                            <div class="dmbox">
-                                <div class="service-icon">
-                                <a class="" href="faq.html#"><i class="dm-icon fa fa-question fa-3x"></i></a>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                            <div className="dmbox">
+                                <div className="service-icon">
+                                <a className="" href="faq.html#"><i className="dm-icon fa fa-question fa-3x"></i></a>
                                 </div>
                                 <h4>1. Knowledge-Base</h4>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s..</p>
                             </div>
                             </div>
 
-                            <div class="col-lg-4 col-md-4 col-sm-12">
-                            <div class="dmbox">
-                                <div class="service-icon">
-                                <a class="" href="faq.html#"><i class="dm-icon fa fa-envelope-o fa-3x"></i></a>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                            <div className="dmbox">
+                                <div className="service-icon">
+                                <a className="" href="faq.html#"><i className="dm-icon fa fa-envelope-o fa-3x"></i></a>
                                 </div>
                                 <h4>2. Report a Bug</h4>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s..</p>
                             </div>
                             </div>
 
-                            <div class="col-lg-4 col-md-4 col-sm-12">
-                            <div class="dmbox">
-                                <div class="service-icon">
-                                <a class="" href="faq.html#"><i class="dm-icon fa fa-random fa-3x"></i></a>
+                            <div className="col-lg-4 col-md-4 col-sm-12">
+                            <div className="dmbox">
+                                <div className="service-icon">
+                                <a className="" href="faq.html#"><i className="dm-icon fa fa-random fa-3x"></i></a>
                                 </div>
                                 <h4>3. Submit a Ticket</h4>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s..</p>
@@ -113,70 +113,70 @@ class FAQ extends Component {
 
                         </div>
 
-                        <div class="row content-panel">
-                        <h2 class="centered">Most Asked Questions</h2>
-                        <div class="col-md-10 col-md-offset-1 mt mb">
-                            <div class="accordion" id="accordion2">
-                            <div class="accordion-group">
-                                <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseOne">
-                                    <em class="glyphicon glyphicon-chevron-right icon-fixed-width"></em>Is the game for free?
+                        <div className="row content-panel">
+                        <h2 className="centered">Most Asked Questions</h2>
+                        <div className="col-md-10 col-md-offset-1 mt mb">
+                            <div className="accordion" id="accordion2">
+                            <div className="accordion-group">
+                                <div className="accordion-heading">
+                                <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseOne">
+                                    <em className="glyphicon glyphicon-chevron-right icon-fixed-width"></em>Is the game for free?
                                     </a>
                                 </div>
-                                <div id="collapseOne" class="accordion-body collapse in">
-                                <div class="accordion-inner">
+                                <div id="collapseOne" className="accordion-body collapse in">
+                                <div className="accordion-inner">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                                     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                 </div>
                                 </div>
                             </div>
-                            <div class="accordion-group">
-                                <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseTwo">
-                                    <em class="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How can I install my server?
+                            <div className="accordion-group">
+                                <div className="accordion-heading">
+                                <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseTwo">
+                                    <em className="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How can I install my server?
                                     </a>
                                 </div>
-                                <div id="collapseTwo" class="accordion-body collapse">
-                                <div class="accordion-inner">
+                                <div id="collapseTwo" className="accordion-body collapse">
+                                <div className="accordion-inner">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                                     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                 </div>
                                 </div>
                             </div>
-                            <div class="accordion-group">
-                                <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseThree">
-                                    <em class="glyphicon glyphicon-chevron-right icon-fixed-width"></em>What are the requirements for running the game?
+                            <div className="accordion-group">
+                                <div className="accordion-heading">
+                                <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseThree">
+                                    <em className="glyphicon glyphicon-chevron-right icon-fixed-width"></em>What are the requirements for running the game?
                                     </a>
                                 </div>
-                                <div id="collapseThree" class="accordion-body collapse">
-                                <div class="accordion-inner">
+                                <div id="collapseThree" className="accordion-body collapse">
+                                <div className="accordion-inner">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                                     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                 </div>
                                 </div>
                             </div>
-                            <div class="accordion-group">
-                                <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseFour">
-                                    <em class="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How to make progress in this game?
+                            <div className="accordion-group">
+                                <div className="accordion-heading">
+                                <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseFour">
+                                    <em className="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How to make progress in this game?
                                     </a>
                                 </div>
-                                <div id="collapseFour" class="accordion-body collapse">
-                                <div class="accordion-inner">
+                                <div id="collapseFour" className="accordion-body collapse">
+                                <div className="accordion-inner">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                                     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                 </div>
                                 </div>
                             </div>
-                            <div class="accordion-group">
-                                <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseFive">
-                                    <em class="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How Can I get Support?
+                            <div className="accordion-group">
+                                <div className="accordion-heading">
+                                <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="faq.html#collapseFive">
+                                    <em className="glyphicon glyphicon-chevron-right icon-fixed-width"></em>How Can I get Support?
                                     </a>
                                 </div>
-                                <div id="collapseFive" class="accordion-body collapse">
-                                <div class="accordion-inner">
+                                <div id="collapseFive" className="accordion-body collapse">
+                                <div className="accordion-inner">
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                                     specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                 </div>
@@ -192,14 +192,14 @@ class FAQ extends Component {
 
                     </section>
 
-                    <footer class="site-footer">
-                    <div class="text-center">
+                    <footer className="site-footer">
+                    <div className="text-center">
                         <p>
                         &copy; Copyrights <strong>Legolas</strong>. All Rights Reserved
                         </p>
 
-                        <a href="faq.html#" class="go-top">
-                        <i class="fa fa-angle-up"></i>
+                        <a href="faq.html#" className="go-top">
+                        <i className="fa fa-angle-up"></i>
                         </a>
                     </div>
                     </footer>
