82bf2a649bd286e98530434a25737d11f622eb59
Luiz Carlos <luizcarlos--1@hotmail.com>
2020-08-19 10:01:13 -0300

    calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -5,56 +5,56 @@ import React, {Component} from 'react';
         return(
             <div>
                 <section id="container">
-                <header class="header black-bg">
-                    <div class="sidebar-toggle-box">
-                        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+                <header className="header black-bg">
+                    <div className="sidebar-toggle-box">
+                        <div className="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
                     </div>
-                    <a href="/" class="logo"><b>Lego<span>las</span></b></a>
+                    <a href="/" className="logo"><b>Lego<span>las</span></b></a>
 
-                    <div class="top-menu">
-                        <ul class="nav pull-right top-menu">
-                        <li><a class="logout" href="/">Logout</a></li>
+                    <div className="top-menu">
+                        <ul className="nav pull-right top-menu">
+                        <li><a className="logout" href="/">Logout</a></li>
                         </ul>
                     </div>
                     </header>
 
                     <aside>
-                    <div id="sidebar" class="nav-collapse ">
-                        <ul class="sidebar-menu" id="nav-accordion">
+                    <div id="sidebar" className="nav-collapse ">
+                        <ul className="sidebar-menu" id="nav-accordion">
 
-                        <li class="mt">
+                        <li className="mt">
                             <a href="/">
-                            <i class="fa fa-dashboard"></i>
+                            <i className="fa fa-dashboard"></i>
                             <span>Dashboard</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
-                            <a class="active" href="javascript:;">
-                            <i class="fa fa-calendar"></i>
+                        <li className="sub-menu">
+                            <a className="active" href="javascript:;">
+                            <i className="fa fa-calendar"></i>
                             <span>Calendar</span>
                             </a>
-                            <ul class="sub">
-                            <li class="active"><a href="/calendar">Events</a></li>
+                            <ul className="sub">
+                            <li className="active"><a href="/calendar">Events</a></li>
                             </ul>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="/gallery">
-                            <i class="fa fa-picture-o"></i>
+                            <i className="fa fa-picture-o"></i>
                             <span>Gallery</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="/contact">
-                            <i class="fa fa-tasks"></i>
+                            <i className="fa fa-tasks"></i>
                             <span>Contact</span>
                             </a>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="javascript:;">
-                            <i class="fa fa-th"></i>
+                            <i className="fa fa-th"></i>
                             <span>Tables</span>
                             </a>
-                            <ul class="sub">
+                            <ul className="sub">
                             <li>
                                 <a href="/">Players</a>
                             </li>
@@ -63,9 +63,9 @@ import React, {Component} from 'react';
                             </li>
                             </ul>
                         </li>
-                        <li class="sub-menu">
+                        <li className="sub-menu">
                             <a href="/faq">
-                            <i class="fa fa-book"></i>
+                            <i className="fa fa-book"></i>
                             <span>FAQ</span>
                             </a>
                         </li>
@@ -74,45 +74,45 @@ import React, {Component} from 'react';
                     </aside>
 
                     <section id="main-content">
-                    <section class="wrapper">
-                        <h3><i class="fa fa-angle-right"></i> Calendar</h3>
+                    <section className="wrapper">
+                        <h3><i className="fa fa-angle-right"></i> Calendar</h3>
 
-                        <div class="row mt">
-                        <aside class="col-lg-3 mt">
-                            <h4><i class="fa fa-angle-right"></i> Event List</h4>
+                        <div className="row mt">
+                        <aside className="col-lg-3 mt">
+                            <h4><i className="fa fa-angle-right"></i> Event List</h4>
                             <div id="external-events">
-                            <div class="external-event label label-theme">CS GO</div>
-                            <div class="external-event label label-success">Dota 2</div>
-                            <div class="external-event label label-info">Overwatch</div>
-                            <div class="external-event label label-warning">Hearthstone</div>
-                            <div class="external-event label label-danger">League of Legends</div>
-                            <div class="external-event label label-default">Fortnite</div>
-                            <div class="external-event label label-theme">Mobile Legends</div>
-                            <div class="external-event label label-info">Minecraft</div>
-                            <div class="external-event label label-success">Lineage</div>
-                            <p class="drop-after">
+                            <div className="external-event label label-theme">CS GO</div>
+                            <div className="external-event label label-success">Dota 2</div>
+                            <div className="external-event label label-info">Overwatch</div>
+                            <div className="external-event label label-warning">Hearthstone</div>
+                            <div className="external-event label label-danger">League of Legends</div>
+                            <div className="external-event label label-default">Fortnite</div>
+                            <div className="external-event label label-theme">Mobile Legends</div>
+                            <div className="external-event label label-info">Minecraft</div>
+                            <div className="external-event label label-success">Lineage</div>
+                            <p className="drop-after">
                                 <input type="checkbox" id="drop-remove"/> Remove After Drop
                             </p>
                             </div>
                         </aside>
-                        <aside class="col-lg-9 mt">
-                            <section class="panel">
-                            <div class="panel-body">
-                                <div id="calendar" class="has-toolbar"></div>
+                        <aside className="col-lg-9 mt">
+                            <section className="panel">
+                            <div className="panel-body">
+                                <div id="calendar" className="has-toolbar"></div>
                             </div>
                             </section>
                         </aside>
                         </div>
                     </section>
                     </section>
-                    <footer class="site-footer">
-                    <div class="text-center">
+                    <footer className="site-footer">
+                    <div className="text-center">
                         <p>
                         &copy; Copyrights <strong>Legolas</strong>. All Rights Reserved
                         </p>
 
-                        <a href="calendar.html#" class="go-top">
-                        <i class="fa fa-angle-up"></i>
+                        <a href="calendar.html#" className="go-top">
+                        <i className="fa fa-angle-up"></i>
                         </a>
                     </div>
                     </footer>
