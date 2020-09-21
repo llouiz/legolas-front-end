13eb6e094f707e080ef3b41cc1141f598c46ba2a
Luiz Carlos <luiz.carlos.techie@gmail.com>
2020-09-21 10:39:16 -0300

    home

---
+++ src/home/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Alert from 'react-s-alert';
+
+class Home extends Component {
+    render() {
+		Alert.success("You're successfully logged in!");
+        return (
+            <div className="home-container">
+                <div className="container">
+                    <div className="graf-bg-container">
+                        <div className="graf-layout">
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                        </div>
+                    </div>
+                    <h1 className="home-title">Welcome to Legolas!</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
