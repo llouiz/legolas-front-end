b8e401be6a89cd90f07dc4b83c943eb3c2cff349
Luiz Carlos <luiz.carlos.techie@gmail.com>
2020-09-23 10:34:22 -0300

    user

---
+++ src/user/login/Login.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { GOOGLE_AUTH_URL, GITHUB_AUTH_URL, ACCESS_TOKEN } from '../../constants';
+import { login } from '../../util/APIUtils';
+import { Link, Redirect } from 'react-router-dom'
+import googleLogo from '../../img/google-logo.png';
+import githubLogo from '../../img/github-logo.png';
+import Alert from 'react-s-alert';
+
+class Login extends Component {
+    componentDidMount() {
+
+        if(this.props.location.state && this.props.location.state.error) {
+            setTimeout(() => {
+                Alert.error(this.props.location.state.error, {
+                    timeout: 5000
+                });
+                this.props.history.replace({
+                    pathname: this.props.location.pathname,
+                    state: {}
+                });
+            }, 100);
+        }
+    }
+
+    render() {
+        if(this.props.authenticated) {
+            return <Redirect
+                to={{
+                pathname: "/index",
+                state: { from: this.props.location }
+            }}/>;
+        }
+
+        return (
+            <div className="login-container">
+                <div className="login-content">
+                    <h1 className="login-title">Login to Legolas</h1>
+                    <SocialLogin />
+                    <div className="or-separator">
+                        <span className="or-text">OR</span>
+                    </div>
+                    <LoginForm {...this.props} />
+                    <span className="signup-link">New user? <Link to="/signup">Sign up!</Link></span>
+                </div>
+            </div>
+        );
+    }
+}
+
+class SocialLogin extends Component {
+    render() {
+        return (
+            <div className="social-login">
+                <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
+                    <img src={googleLogo} alt="Google" /> Log in with Google</a>
+                <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
+                    <img src={githubLogo} alt="Github" /> Log in with Github</a>
+            </div>
+        );
+    }
+}
+
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName] : inputValue
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const loginRequest = Object.assign({}, this.state);
+
+        login(loginRequest)
+        .then(response => {
+            localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+            //this.props.history.push("/");
+            window.location.href = "/index";
+        }).catch(error => {
+            Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-item">
+                    <input type="email" name="email"
+                        className="ctrl" placeholder="Email"
+                        value={this.state.email} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <input type="password" name="password"
+                        className="ctrl" placeholder="Password"
+                        value={this.state.password} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <button type="submit" className="btn btn-block btn-primary">Login</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default Login
---
+++ src/user/signup/Signup.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './Signup.css';
+import { Link, Redirect } from 'react-router-dom'
+import { GOOGLE_AUTH_URL, GITHUB_AUTH_URL } from '../../constants';
+import { signup } from '../../util/APIUtils';
+import googleLogo from '../../img/google-logo.png';
+import githubLogo from '../../img/github-logo.png';
+import Alert from 'react-s-alert';
+
+class Signup extends Component {
+    render() {
+        if(this.props.authenticated) {
+            return <Redirect
+                to={{
+                pathname: "/",
+                state: { from: this.props.location }
+            }}/>;
+        }
+
+        return (
+            <div className="signup-container">
+                <div className="signup-content">
+                    <h1 className="signup-title">Sign up to Legolas</h1>
+                    <SocialSignup />
+                    <div className="or-separator">
+                        <span className="or-text">OR</span>
+                    </div>
+                    <SignupForm {...this.props} />
+                    <span className="login-link">Have an account already ? <Link to="/login">Login!</Link></span>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class SocialSignup extends Component {
+    render() {
+        return (
+            <div className="social-signup">
+                <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
+                    <img src={googleLogo} alt="Google" /> Sign up with Google</a>
+                <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
+                    <img src={githubLogo} alt="Github" /> Sign up with Github</a>
+            </div>
+        );
+    }
+}
+
+class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName] : inputValue
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const signUpRequest = Object.assign({}, this.state);
+
+        signup(signUpRequest)
+        .then(response => {
+            Alert.success("You're successfully registered. Please login to continue!");
+            this.props.history.push("/login");
+        }).catch(error => {
+            Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-item">
+                    <input type="text" name="name"
+                        className="ctrl" placeholder="Name"
+                        value={this.state.name} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <input type="email" name="email"
+                        className="ctrl" placeholder="Email"
+                        value={this.state.email} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <input type="password" name="password"
+                        className="ctrl" placeholder="Password"
+                        value={this.state.password} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <button type="submit" className="btn btn-block btn-primary" >Sign Up</button>
+                </div>
+            </form>
+
+        );
+    }
+}
+
+export default Signup
